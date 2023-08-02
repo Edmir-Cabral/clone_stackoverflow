4beda3d4bad261fd706eabd6fa11c3cb9a014c55
Edmir Souza <edmir.souza@capgemini.com>
2023-08-02 16:01:29 -0300

    projeto criado

---
+++ clonestackoverflow/src/main/java/com/fulltechjava/clonestackoverflow/ClonestackoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.fulltechjava.clonestackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonestackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonestackoverflowApplication.class, args);
+	}
+
+}
---
+++ clonestackoverflow/src/test/java/com/fulltechjava/clonestackoverflow/ClonestackoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulltechjava.clonestackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonestackoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
