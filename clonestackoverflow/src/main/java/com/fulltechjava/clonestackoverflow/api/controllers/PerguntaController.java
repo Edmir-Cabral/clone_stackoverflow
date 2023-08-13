120b8597954714c3b97f21de2d5dfaa5c33c0ced
Rafael Pino <rafael.pino@capgemini.com>
2023-08-12 23:40:37 -0300

    feature: criacao de api pergunta

--- clonestackoverflow/src/main/java/com/fulltechjava/clonestackoverflow/api/controllers/PerguntaController.java
+++ clonestackoverflow/src/main/java/com/fulltechjava/clonestackoverflow/api/controllers/PerguntaController.java
@@ -1,5 +1,58 @@
 package com.fulltechjava.clonestackoverflow.api.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fulltechjava.clonestackoverflow.models.Pergunta;
+import com.fulltechjava.clonestackoverflow.services.PerguntaService;
+
+@RestController
+@RequestMapping("/pergunta")
 public class PerguntaController {
 
+	@Autowired
+	private PerguntaService perguntaService;
+
+	public PerguntaController(PerguntaService _perguntaService) {
+		this.perguntaService = _perguntaService;
+	}
+
+	@GetMapping("/")
+	public List<Pergunta> getPerguntas() {
+		return perguntaService.getAllPerguntas();
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Pergunta> postPergunta(@RequestBody Pergunta pergunta) {
+		pergunta = perguntaService.postPergunta(pergunta);
+		URI uri = ServletUriComponentsBuilder.
+		fromCurrentRequest().
+		path("/{id}").
+		buildAndExpand(pergunta.getId()).toUri();
+		return ResponseEntity.created(uri).body(pergunta);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable int id){
+		perguntaService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Pergunta> update(@PathVariable int id, @RequestBody Pergunta pergunta){
+		 pergunta = perguntaService.update(id, pergunta);
+		 return ResponseEntity.ok().body(pergunta);
+	}
 }
--- clonestackoverflow/src/main/java/com/fulltechjava/clonestackoverflow/services/PerguntaService.java
+++ clonestackoverflow/src/main/java/com/fulltechjava/clonestackoverflow/services/PerguntaService.java
@@ -1,8 +1,44 @@
 package com.fulltechjava.clonestackoverflow.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import com.fulltechjava.clonestackoverflow.models.Pergunta;
+import com.fulltechjava.clonestackoverflow.repositories.PerguntaRepository;
 
 @Service
 public class PerguntaService {
 
+	private PerguntaRepository perguntaRepository;
+	
+	public PerguntaService(PerguntaRepository _perguntaRepository) {
+		this.perguntaRepository = _perguntaRepository;
+	}
+	
+	public List<Pergunta> getAllPerguntas(){
+		return perguntaRepository.findAll();
+	}
+
+	public Pergunta postPergunta(Pergunta pergunta) {
+		return perguntaRepository.save(pergunta);
+	}
+	
+	public void delete (int id) {
+		perguntaRepository.deleteById(id);
+	}
+	
+	public Pergunta update(int id, Pergunta pergunta) {
+		Pergunta entity = perguntaRepository.getReferenceById(id);
+		updateData(entity, pergunta);
+		return perguntaRepository.save(entity);
+	}
+
+	private void updateData(Pergunta entity, Pergunta pergunta) {
+		entity.setTitulo_pergunta(pergunta.getDescricao_pergunta());
+		entity.setDescricao_pergunta(pergunta.getDescricao_pergunta());
+		entity.setData_pergunta(pergunta.getData_pergunta());		
+	}
 }
